f79524e9245e1baefde141d8d471c0d17c43138c
dacevedom <d.acevedom1993@gmail.com>
2026-03-13 11:47:43 -0300

    Multidimensional array creation and reading

--- Program.vb
+++ Program.vb
@@ -20,5 +20,24 @@ Module Program
         'Mostramos el valor en la consola
         Console.WriteLine(miArray(3))
         Console.Read()
+
+        Dim miArrayMulti(2, 2) As String
+        miArrayMulti(0, 0) = "Antonio"
+        miArrayMulti(0, 1) = "35"
+        miArrayMulti(0, 2) = "65465321321"
+
+        miArrayMulti(1, 0) = "Francisco"
+        miArrayMulti(1, 1) = "30"
+        miArrayMulti(1, 2) = "5654654"
+
+        miArrayMulti(2, 0) = "Juan"
+        miArrayMulti(2, 1) = "20"
+        miArrayMulti(2, 2) = "0000000"
+
+        'Mostramos el valor en la consola
+        Console.WriteLine(miArrayMulti(2, 0) & " " &
+            miArrayMulti(2, 1) & " " &
+            miArrayMulti(2, 2))
+        Console.ReadKey()
     End Sub
 End Module
